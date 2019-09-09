15857953d5ad5df87c94008c5383f595721f7741
Yuri Shvyrkov <shvyrkov@yandex.ru>
2019-09-09 23:05:12 +0400

    Добавление комментария в чат

--- future_test/get_comment.php
+++ future_test/get_comment.php
@@ -1,35 +1,39 @@
-<?php 
-session_start(); //Запуск сессии
-include 'bd.php'; // Подключение БД
+<?php // Внесение данных в БД.
+require_once 'bd_connection.php'; // Подключение БД
+require_once 'safe_insertion.php';// Функция mysql_entities_fix_string() для предотвращения атак внедрения SQL и XSS
 
 if(!$_POST['form_name']) // Проверка ввода имени
-    exit ('<meta http-equiv="refresh" content="3;URL=index.php"> 
+    exit ('<meta http-equiv="refresh" content="1;URL=index.php"> 
         <meta charset="utf-8">
         <script>alert("Укажите имя!")</script>');
         
 if(!$_POST['form_comment']) // Проверка ввода комментария
-    exit ('<meta http-equiv="refresh" content="3;URL=index.php"> 
+    exit ('<meta http-equiv="refresh" content="1;URL=index.php"> 
         <meta charset="utf-8">
         <script>alert("Внесите комментарий!")</script>');
         
-$user_name = htmlspecialchars(stripslashes($_POST['form_name'])); // Имя из формы + "очистка".
-$user_comment = htmlspecialchars(stripslashes($_POST['form_comment'])); // Комментарий из формы + "очистка".
+$user_name = mysql_entities_fix_string($connection_bd, $_POST['form_name']); // Имя из формы + "очистка".
+$user_comment = mysql_entities_fix_string($connection_bd, $_POST['form_comment']); // Комментарий из формы + "очистка".
 $user_date = date("d.m.y"); // Текущая дата
 $user_time = date("H:i"); // Текущее время
    
-//echo("Текущее время: $user_time и дата: $user_date .<br>");//test
-//echo $user_name;//test
+// Внесение записи в таблицу SQL.  
+$query = "INSERT INTO `users`(`name`, `time`, `date`, `comment`) 
+                    VALUES ('$user_name','$user_time','$user_date','$user_comment')";
 
-// Внесение записи в таблицу SQL.   
-    $mysqli->query("INSERT INTO `users`(`name`, `time`, `date`, `comment`) 
-                        VALUES ('$user_name','$user_time','$user_date','$user_comment')"); 
-    if(!$mysqli) { // Если нет доступа к таблице 
-        echo 'Не могу загрузить данные. <br> Проверьте подключение к БД.
-        <br><br><a href="index.php"> <button>Вернуться на главную.</button> </a>';
-        exit;
-    }
+require_once 'bd_request.php'; // Запрос в БД
+       
+if (!$result) // Если запись в таблицу не прошла
+{
+    echo "INSERT failed<br><br>";
+}
+   
+// Сброс переменных и объектов.   
 unset($_POST['form_name']); // Удаление значения переменной   
-unset($_POST['$user_comment']); // Удаление значения переменной   
-//echo '$_POST[\'form_name\']: '.$_POST['form_name'];//test
+unset($_POST['$user_comment']); // Удаление значения переменной  
+unset($result);
+//$result->close();// Если используется с SELECT...
+$connection_bd->close();
+
 echo '<meta http-equiv="refresh" content="0;URL=index.php">';
 ?>
