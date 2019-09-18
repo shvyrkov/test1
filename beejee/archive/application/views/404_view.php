816ee4ba78d3f335b9cf30a1d771af6998661237
Yuri Shvyrkov <shvyrkov@yandex.ru>
2019-09-18 17:49:29 +0400

    Удаление /archive/

--- beejee/archive/application/views/404_view.php
+++
@@ -1,4 +0,0 @@
-<h1>404</h1>
-<p>
-<img src="/beejee/images/404.png">
-</p>
--- beejee/archive/application/views/fail_view.php
+++
@@ -1,7 +0,0 @@
-<h1> 
-Сбой в программе
-</h1>
-
-<button type="button" class="btn btn-outline-info">
-    <a  href="/beejee/">К списку задач</a>
-</button>
